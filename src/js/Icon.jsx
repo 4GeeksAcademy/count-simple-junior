db2ae5ef4c7d3f1ee2c25cf1fb0ea6c284518420
Junior Sequera <35280577+junseiken@users.noreply.github.com>
2025-08-31 04:29:04 +0000

    cambios

---
+++ src/js/Icon.jsx
@@ -0,0 +1,7 @@
+/* FontAwesone */
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faClock } from '@fortawesome/free-solid-svg-icons';
+
+export function ClockIcon() {
+  return <FontAwesomeIcon icon={faClock} style={{ fontSize: "40px"}} />;
+}
--- src/js/main.jsx
+++ src/js/main.jsx
@@ -5,14 +5,29 @@ import ReactDOM from 'react-dom/client'
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap"
 
+// icons
+import { ClockIcon } from './Icon.jsx';
 // index.css'
 import '../styles/index.css'
 
 // components
-import Home from './components/Home';
+function CounterSimple(props) {
+  return (
+    <div className='containerCounter'>
+      <div className='calendar'>
+        <ClockIcon />
+      </div>
+      <div className='cinco'></div>
+      <div className='cuatro'></div>
+      <div className='tres'></div>
+      <div className='dos'></div>
+      <div className='uno'></div>
+    </div>
+  );
+}
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Home/>
+    <CounterSimple />
   </React.StrictMode>,
 )
