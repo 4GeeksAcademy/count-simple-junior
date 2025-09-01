ddf5b3906ff9c722c6f29fc4dd10a8e648981f40
Junior Sequera <35280577+junseiken@users.noreply.github.com>
2025-09-01 01:59:31 +0000

    agg finish

--- src/js/main.jsx
+++ src/js/main.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import ReactDOM from 'react-dom/client'
 
 //Bootstrap
@@ -11,17 +11,29 @@ import { ClockIcon } from './Icon.jsx';
 import '../styles/index.css'
 
 // components
-function CounterSimple(props) {
+function CounterSimple() {
+  const [counter, setCounter] = useState(0);
+  useEffect(() => {
+    const interval = setInterval(() => setCounter(c => c + 1), 1000);
+    return () => clearInterval(interval);
+  }, []);
+
+  const digitUno = Math.floor((counter / 1) % 10);
+  const digitDos = Math.floor((counter / 10) % 10);
+  const digitTres = Math.floor((counter / 100) % 10);
+  const digitCuatro = Math.floor((counter / 1000) % 10);
+  const digitCinco = Math.floor((counter / 10000) % 10);
+
   return (
     <div className='containerCounter'>
       <div className='calendar'>
         <ClockIcon />
       </div>
-      <div className='cinco'></div>
-      <div className='cuatro'></div>
-      <div className='tres'></div>
-      <div className='dos'></div>
-      <div className='uno'></div>
+      <div className='cinco'>{digitCinco}</div>
+      <div className='cuatro'>{digitCuatro}</div>
+      <div className='tres'>{digitTres}</div>
+      <div className='dos'>{digitDos}</div>
+      <div className='uno'>{digitUno}</div>
     </div>
   );
 }
